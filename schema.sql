d213b143549827ebae36e8f4d307161de662d053
shownok-afk <shownok@gmail.com>
2021-08-10 17:51:05 -0400

    update schema.sql

---
+++ schema.sql
@@ -0,0 +1,58 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+-- Creating tables Employees
+CREATE TABLE employees (
+     emp_no int NOT NULL,
+     birth_date date NOT NULL,
+	 first_name varchar(40) NOT NULL,
+	 last_name varchar(40) NOT NULL,
+	 gender varchar (1) NOT NULL,
+	 hire_date date NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+-- Creating tables dept_manager
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+-- Creating tables salaries
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  dept_no varchar(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+-- Creating tables dept_emp
+CREATE TABLE dept_emp (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+-- Creating tables Titles
+CREATE TABLE Titles (
+  emp_no INT NOT NULL,
+  title varchar(40) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+-- Query to check a table departments 
+SELECT * FROM departments;
